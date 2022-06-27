9f7e6a28d2397f881f78dc21b83b287ad98bcb2d
Alina Lemishko <aliska.nofear@gmail.com>
2022-06-27 22:05:01 +0300

    finnished hw-01

---
+++ src/components/FriendList/FriendListItem.js
@@ -0,0 +1,20 @@
+import s from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={s.item}>
+      <span className={isOnline ? s.statusGreen : s.statusRed}></span>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendListItem;
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,20 @@
+import s from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={s.statistics}>
+      {title ? <h2 className={s.title}>{title}</h2> : null}
+
+      <ul className={s.statList}>
+        {stats.map(stat => (
+          <li key={stat.id} className={s.item}>
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
